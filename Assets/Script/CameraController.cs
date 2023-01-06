1298fca501493033625b02c310fa6ba17dae1d9f
Abe Azka Official YT <azka.jsiswanto@gmail.com>
2023-01-06 22:03:52 +0700

    0.10 V2

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -7,9 +7,11 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Transform player;
     
 
+
     private void Update()
     {
         transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 
+    
 }
--- Assets/Script/PlayerLife.cs
+++ Assets/Script/PlayerLife.cs
@@ -41,4 +41,6 @@ public class PlayerLife : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    
 }
