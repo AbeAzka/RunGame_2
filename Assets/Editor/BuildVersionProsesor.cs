26ddc82926c1a9730b671ade6c8813d068b1c715
Abe Azka Official YT <azka.jsiswanto@gmail.com>
2023-01-06 23:16:34 +0700

    1.0 v1

--- Assets/Editor/BuildVersionProsesor.cs
+++ Assets/Editor/BuildVersionProsesor.cs
@@ -27,7 +27,7 @@ public class BuildVersionProsesor : IPreprocessBuildWithReport
     {
         if (float.TryParse(version, out float versionNumber))
         {
-            float newVersion = versionNumber = 0.10f;
+            float newVersion = versionNumber = 1.0f;
             string date = DateTime.Now.ToString("d");
 
             PlayerSettings.bundleVersion = string.Format("Version {0} - [{1}]", newVersion, date);
