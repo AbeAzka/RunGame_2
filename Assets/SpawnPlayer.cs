f103645fe0aff822453e3bc7609346669d152d74
Abe Azka Official YT <azka.jsiswanto@gmail.com>
2023-01-07 14:29:56 +0700

    P

---
+++ Assets/SpawnPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject SceneCamera;
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    private void Start()
+    {
+        Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
+        SceneCamera.SetActive(false);
+    }
+
+}
