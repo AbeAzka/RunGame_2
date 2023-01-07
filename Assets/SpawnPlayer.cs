040e7a40ae2b8af99129891c1585d861686d7d18
Abe Azka Official YT <azka.jsiswanto@gmail.com>
2023-01-07 15:34:39 +0700

    1.1.4

--- Assets/SpawnPlayer.cs
+++ Assets/SpawnPlayer.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 
 public class SpawnPlayer : MonoBehaviour
 {
     public GameObject playerPrefab;
     public GameObject SceneCamera;
+    public Text PingText;
 
     public float minX;
     public float maxX;
@@ -20,4 +22,20 @@ public class SpawnPlayer : MonoBehaviour
         SceneCamera.SetActive(false);
     }
 
+    private void Update()
+    {
+        PingText.text = "Ping: " + PhotonNetwork.GetPing();
+        if (PingText.text.Length >= 9)
+        {
+            PingText.color = Color.red;
+        }
+        if (PingText.text.Length == 8)
+        {
+            PingText.color = Color.yellow;
+        }
+        if (PingText.text.Length == 7)
+        {
+            PingText.color = Color.green;
+        }
+    }
 }
