9677bb69ff9d7a4989042af70dd475bdb3217abb
Abe Azka Official YT <azka.jsiswanto@gmail.com>
2023-01-08 20:31:43 +0700

    1.4 Main

--- Assets/Script/Multiplayer/RoomListing.cs
+++ Assets/Script/Multiplayer/RoomListing.cs
@@ -8,8 +8,12 @@ public class RoomListing : MonoBehaviour
 {
     [SerializeField] private Text text;
 
+    public RoomInfo RoomInfo { get; private set; }
+
+
     public void SetRoomInfo(RoomInfo roomInfo)
     {
+        RoomInfo = roomInfo;
         text.text = roomInfo.MaxPlayers + ", " + roomInfo.Name;
     }
 }
--- Assets/Script/Multiplayer/RoomListingMenu.cs
+++ Assets/Script/Multiplayer/RoomListingMenu.cs
@@ -9,14 +9,30 @@ public class RoomListingMenu : MonoBehaviourPunCallbacks
     [SerializeField] private Transform content;
     [SerializeField] private RoomListing roomListing;
 
+    private List<RoomListing> listings = new List<RoomListing>();
+
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
         foreach (RoomInfo info in roomList)
         {
+            if (info.RemovedFromList)
+            {
+                int index = listings.FindIndex(x => x.RoomInfo.Name == info.Name);
+                if (index != -1)
+                {
+                    Destroy(listings[index].gameObject);
+                    listings.RemoveAt(index);
+                }
+            }
+            else
+            {
+
+            }
             RoomListing listing = Instantiate(roomListing, content);
             if (listing != null)
             {
                 listing.SetRoomInfo(info);
+                listings.Add(listing);
             }
         }
 
