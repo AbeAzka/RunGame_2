3d347fea36ae5a231f32cf3593fba701da54d8d5
Abe Azka Official YT <azka.jsiswanto@gmail.com>
2023-02-18 18:29:18 +0700

    1.5.1

--- Assets/Editor/BuildVersionProsesor.cs
+++ Assets/Editor/BuildVersionProsesor.cs
@@ -27,7 +27,7 @@ public class BuildVersionProsesor : IPreprocessBuildWithReport
     {
         if (float.TryParse(version, out float versionNumber))
         {
-            float newVersion = versionNumber = 1.4f;
+            float newVersion = versionNumber = 1.5f;
             string date = DateTime.Now.ToString("d");
 
             PlayerSettings.bundleVersion = string.Format("Version {0} - [{1}]", newVersion, date);
---
+++ Assets/Script/LevelUnlock1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUnlock1 : MonoBehaviour
+{
+    public Button[] levelButtons;
+
+    void Start()
+    {
+        
+    }
+}
