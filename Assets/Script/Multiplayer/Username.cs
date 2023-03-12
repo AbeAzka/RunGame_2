04134e172f39714debc480eb3628818f0f64ff5c
Abe Azka Official YT <azka.jsiswanto@gmail.com>
2023-03-12 12:46:39 +0700

    2.1 #3

--- Assets/Script/Multiplayer/Username.cs
+++ Assets/Script/Multiplayer/Username.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using PlayFab;
+using PlayFab.ClientModels;
 
 public class Username : MonoBehaviour
 {
@@ -12,6 +14,11 @@ public class Username : MonoBehaviour
 
     void Start()
     {
+        if (string.IsNullOrEmpty(PlayFabSettings.TitleId))
+        {
+            PlayFabSettings.TitleId = "FE39E";
+        }
+
         if (PlayerPrefs.GetString("Username") == "" || PlayerPrefs.GetString("Username") == null)
         {
             UsernamePage.SetActive(true);
@@ -26,6 +33,7 @@ public class Username : MonoBehaviour
         }
     }
 
+
     public void OpenUsernamePage()
     {
         UsernamePage.SetActive(true);
@@ -37,8 +45,30 @@ public class Username : MonoBehaviour
         UsernamePage.SetActive(false);
         Time.timeScale = 1f;
     }
+
+    void OnDataReceived(GetUserDataResult result)
+    {
+        Debug.Log("Received user data!");
+        if (result.Data != null && result.Data.ContainsKey("Username") && result.Data.ContainsKey("UserInfo") && result.Data.ContainsKey("MyUsername"))
+        {
+            PhotonNetwork.NickName = inputField.text;
+            PlayerPrefs.GetString("Username", inputField.text);
+            MyUsername.text = inputField.text;
+        }
+    }
     public void SaveUsername()
     {
+        var request = new UpdateUserDataRequest
+        {
+            Data = new Dictionary<string, string>()
+            {
+                {"Username", PhotonNetwork.NickName = inputField.text},
+                {"UserInfo", PlayerPrefs.GetString("Username", inputField.text) },
+                {"MyUsername",  MyUsername.text = inputField.text }
+            }
+        };
+        PlayFabClientAPI.UpdateUserData(request, OnDataSend, OnError);
+
         PhotonNetwork.NickName = inputField.text;
 
         PlayerPrefs.GetString("Username", inputField.text);
@@ -47,4 +77,21 @@ public class Username : MonoBehaviour
 
         UsernamePage.SetActive(false);
     }
+
+    void OnDataSend(UpdateUserDataResult result)
+    {
+        Debug.Log("Successful user data send!");
+    }
+    void OnError(PlayFabError error)
+    {
+        Debug.Log("Error with PlayFab");
+        Debug.Log(error.GenerateErrorReport());
+    }
+
+
+
+
+
+
+
 }
